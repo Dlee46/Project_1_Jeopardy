1944c59aa48ffdd4bfb8067dfae04e4d81d22ca4
dlee46 <dannyl.dhl@gmail.com>
2018-05-22 13:44:58 -0400

    added modal to grid

--- JS/main.js
+++ JS/main.js
@@ -1,36 +1,4 @@
-function myFunction() {
-    var person = prompt("Please enter your name:", "Answer");
-    if (person == null || person == "") {
-        alert("User cancelled the prompt.");
-    } else {
-        alert("Hello " + person + "! How are you today?");
-    }
-    
-}
-function myFunction() {
-    var person = prompt("Please enter your name:", "Answer");
-    if (person == null || person == "") {
-        alert("User cancelled the prompt.");
-    } else {
-        alert("Hello " + person + "! How are you today?");
-    }
-    
-}
-function myFunction() {
-    var person = prompt("Please enter your name:", "Answer");
-    if (person == null || person == "") {
-        alert("User cancelled the prompt.");
-    } else {
-        alert("Hello " + person + "! How are you today?");
-    }
-    
-}
-function myFunction() {
-    var person = prompt("Please enter your name:", "Answer");
-    if (person == null || person == "") {
-        alert("User cancelled the prompt.");
-    } else {
-        alert("Hello " + person + "! How are you today?");
-    }
-    
-}
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+
