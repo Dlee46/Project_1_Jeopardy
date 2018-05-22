58a1b0d886084252591788e60acaa83c00b4b264
dlee46 <dannyl.dhl@gmail.com>
2018-05-22 10:56:47 -0400

    folders

---
+++ JS/main.js
@@ -0,0 +1,36 @@
+function myFunction() {
+    var person = prompt("Please enter your name:", "Answer");
+    if (person == null || person == "") {
+        alert("User cancelled the prompt.");
+    } else {
+        alert("Hello " + person + "! How are you today?");
+    }
+    
+}
+function myFunction() {
+    var person = prompt("Please enter your name:", "Answer");
+    if (person == null || person == "") {
+        alert("User cancelled the prompt.");
+    } else {
+        alert("Hello " + person + "! How are you today?");
+    }
+    
+}
+function myFunction() {
+    var person = prompt("Please enter your name:", "Answer");
+    if (person == null || person == "") {
+        alert("User cancelled the prompt.");
+    } else {
+        alert("Hello " + person + "! How are you today?");
+    }
+    
+}
+function myFunction() {
+    var person = prompt("Please enter your name:", "Answer");
+    if (person == null || person == "") {
+        alert("User cancelled the prompt.");
+    } else {
+        alert("Hello " + person + "! How are you today?");
+    }
+    
+}
